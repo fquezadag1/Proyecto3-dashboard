6e243ffe8271a35261eee5735577a7ce686ca128
Felipe Quezada <fquezadag1@gmail.com>
2023-08-25 18:09:09 -0400

    commit4-grbarra

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ async function renderData() {
 
     const grLinea = await obtenerdata('http://ergast.com/api/f1/current/drivers/alonso/results.json');
     const grCircular = await obtenerdata('http://ergast.com/api/f1/current/driverStandings.json');
+    const grBarra = await obtenerdata('http://ergast.com/api/f1/current/constructorStandings.json');
     
     
     let datagrLinea = grLinea.data.MRData.RaceTable.Races;
     let datagrCircular= grCircular.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    
+    let datagrBarra= grBarra.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings;
     
     let circuitos = datagrLinea.map(circuito => circuito.raceName)
     let posiciones = datagrLinea.map(posicion => posicion.Results[0].points)
@@ -16,8 +17,12 @@ async function renderData() {
     let puntosPilotos = datagrCircular.map(puntosPiloto => puntosPiloto.points)
     let nombrePilotos = datagrCircular.map(nombrePiloto =>nombrePiloto.Driver.familyName)
 
+    let puntosEquipos = datagrBarra.map(puntosEquipo => puntosEquipo.points)
+    let equipos = datagrBarra.map(equipo => equipo.Constructor.name)
+    
     const linea = document.getElementById('grlinea');
     const circular = document.getElementById('grcircular');
+    const barra = document.getElementById('grbarra');
 
     new Chart(linea, {
         type: 'line',
@@ -87,6 +92,62 @@ async function renderData() {
         }
     });
 
+    new Chart(barra, {
+        type: 'bar',
+        data: {
+          labels: equipos,
+          datasets: [{
+            label: 'Puntos',
+            data: puntosEquipos,
+            borderWidth: 1,
+            backgroundColor: [
+                'rgba(0, 0, 204, 0.5)',
+                'rgba(160, 160, 160, 0.5)',
+                'rgba(0, 51, 0, 0.5)',
+                'rgba(255, 0, 0, 0.5)',
+                'rgba(255, 128, 0, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(3, 11, 255, 0.5)',
+                'rgba(255, 255, 255, 0.5)',
+                'rgba(153, 0, 0, 0.5)',
+                'rgba(255, 255, 255, 0.5)'
+            ],
+            borderColor: [
+                'rgb(0, 0, 204)',
+                'rgb(160, 160, 160)',
+                'rgb(0, 51, 0)',
+                'rgb(255, 0, 0)',
+                'rgb(255, 128, 0)',
+                'rgb(153, 102, 255)',
+                'rgb(255, 255, 255)',
+                'rgb(255, 0, 0)',
+                'rgb(255, 255, 255)',
+                'rgb(0, 76, 153)'
+            ]
+          }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color:'white'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+      });
 }
 renderData(); 
 
