94acd6c649761fd8798e3b39441fd83a6d54ced3
Felipe Quezada <fquezadag1@gmail.com>
2023-08-25 16:54:45 -0400

    commit-3-grcircuclar

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ import { obtenerdata } from "./axios.js";
 async function renderData() {
 
     const grLinea = await obtenerdata('http://ergast.com/api/f1/current/drivers/alonso/results.json');
+    const grCircular = await obtenerdata('http://ergast.com/api/f1/current/driverStandings.json');
     
     
     let datagrLinea = grLinea.data.MRData.RaceTable.Races;
+    let datagrCircular= grCircular.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
     
     
     let circuitos = datagrLinea.map(circuito => circuito.raceName)
     let posiciones = datagrLinea.map(posicion => posicion.Results[0].points)
 
+    let puntosPilotos = datagrCircular.map(puntosPiloto => puntosPiloto.points)
+    let nombrePilotos = datagrCircular.map(nombrePiloto =>nombrePiloto.Driver.familyName)
+
     const linea = document.getElementById('grlinea');
-    
+    const circular = document.getElementById('grcircular');
 
     new Chart(linea, {
         type: 'line',
@@ -46,6 +51,36 @@ async function renderData() {
                     beginAtZero: true,
                     ticks: {
                         color:'white'
+                    }
+                }
+            }
+        }
+    });
+
+    new Chart(circular, {
+        type: 'pie',
+        data: {
+            labels: [
+                nombrePilotos[4],
+                nombrePilotos[6]
+                
+            ],  
+            datasets: [{
+                label: 'Puntos',
+                data: [puntosPilotos[4],puntosPilotos[6]],
+                backgroundColor: [
+                    'rgb(255, 0, 0)',
+                    'rgb(255, 255, 0)'
+                ],
+                hoverOffset: 4,
+            }],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                  labels: {
+                    color: 'white'
                     }
                 }
             }
