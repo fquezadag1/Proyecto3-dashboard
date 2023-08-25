1680a665c25bbb4926bbbfd42dd57fcdd0317f5b
Felipe Quezada <fquezadag1@gmail.com>
2023-08-25 13:38:46 -0400

    commmit-2-grlinea

--- script.js
+++ script.js
@@ -2,7 +2,56 @@ import { obtenerdata } from "./axios.js";
 
 async function renderData() {
 
+    const grLinea = await obtenerdata('http://ergast.com/api/f1/current/drivers/alonso/results.json');
     
+    
+    let datagrLinea = grLinea.data.MRData.RaceTable.Races;
+    
+    
+    let circuitos = datagrLinea.map(circuito => circuito.raceName)
+    let posiciones = datagrLinea.map(posicion => posicion.Results[0].points)
+
+    const linea = document.getElementById('grlinea');
+    
+
+    new Chart(linea, {
+        type: 'line',
+        data: {
+            labels: circuitos,
+            datasets: [{
+                label: 'Puntos Ganados por Carrera',
+                data: posiciones,
+                fill: false,
+                borderColor: 'rgb(18, 151, 58)',
+                backgroundColor: 'rgb(255, 128, 0)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                  labels: {
+                    color: 'white'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: 'white'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color:'white'
+                    }
+                }
+            }
+        }
+    });
+
 }
 renderData(); 
 
